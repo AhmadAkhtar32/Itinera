1a729ee043725a52f99e0191b377a9223a1650f5
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-11-24 23:44:17 +0500

    starting with Header File

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+        {/* LOGO */}
+
+        {/* Menu Options */}
+
+        {/* Get Started button  */}
+    </div>
   )
 }
 
