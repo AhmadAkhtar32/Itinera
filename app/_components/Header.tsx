3224c7bdc1390fa809f636a6021be9af828766ad
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-11-24 23:57:18 +0500

    added logo and title to header

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 
 function Header() {
-  return (
-    <div>
-        {/* LOGO */}
+    return (
+        <div>
+            {/* LOGO */}
+            <div>
+                <img src="/logo.svg" alt="logo" style={{ width: 50, height: 50 }} />
+                <h2>Itinera</h2>
+            </div>
 
-        {/* Menu Options */}
 
-        {/* Get Started button  */}
-    </div>
-  )
+            {/* Menu Options */}
+
+            {/* Get Started button  */}
+        </div>
+    )
 }
 
 export default Header
