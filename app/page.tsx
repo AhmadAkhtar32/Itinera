49f237eb42e122db8f17150f4d409146676904d4
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-11-24 22:47:49 +0500

    adding custom font

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
     <div >
       <h1>Welcome TO FYP ! </h1>
+      <Button >Subscribe</Button>
     </div>
   );
 }
