8fa63607095a2bed51d1af3964be9461bcb315c8
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-11-25 01:12:20 +0500

    commit

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 function Hero() {
     return (
-        <div className="mt-24 flex items-center">
+        <div className="mt-24 flex items-center flex justify-center">
             {/* Content  */}
-            <div className='flex items-center'>
-                <h1 className='text-xl md:text-5xl font-bold'>Hey! <span className='text-primary'>Itinera</span> at your service — plan smarter, travel better.</h1>
+            <div className='max-w-3xl w-full text-center '>
+                <h1 className='text-xl md:text-5xl font-bold'>Hey! <span className='text-primary'>Itinera</span> at your service — plan smarter-travel better✈️</h1>
+                <p>Tell me what you need — I’ll plan the flights, hotels, and everything else in seconds.</p>
             </div>
 
             {/* Input Box  */}
