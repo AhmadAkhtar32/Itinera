1350fcf48e7b8f7ca52555b4bfc7c07d9125068f
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-11-25 01:02:37 +0500

    setting up content section in Hero file

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -4,13 +4,16 @@ function Hero() {
     return (
         <div className="mt-24 flex items-center">
             {/* Content  */}
+            <div className='flex items-center'>
+                <h1 className='text-xl md:text-5xl font-bold'>Hey! <span className='text-primary'>Itinera</span> at your service — plan smarter, travel better.</h1>
+            </div>
 
             {/* Input Box  */}
 
             {/* Suggestion List  */}
 
             {/* Video section  */}
-            </div>
+        </div>
     )
 }
 
