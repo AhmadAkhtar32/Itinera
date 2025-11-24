3b0783db7f75ebd2ab55de50728037384b0ef52b
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-11-25 01:29:51 +0500

    text-area fixed

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,15 +1,21 @@
+import { Textarea } from '@/components/ui/textarea'
 import React from 'react'
 
 function Hero() {
     return (
-        <div className="mt-24 flex items-center flex justify-center">
+        <div className="mt-24 flex flex-col items-center justify-center gap-10">
             {/* Content  */}
-            <div className='max-w-3xl w-full text-center '>
+            <div className='max-w-3xl w-full text-center space-y-6'>
                 <h1 className='text-xl md:text-5xl font-bold'>Hey! <span className='text-primary'>Itinera</span> at your service — plan smarter-travel better✈️</h1>
-                <p>Tell me what you need — I’ll plan the flights, hotels, and everything else in seconds.</p>
+                <p className='text-lg'>Tell me what you need — I’ll plan the flights, hotels, and everything else in seconds.</p>
             </div>
 
             {/* Input Box  */}
+            <div>
+                <div className='border rounded-2xl p-4 shadow'>
+                    <Textarea placeholder="Let's Plan A Trip Together 😊" className='w-180 h-28 bg-transparent border-none focus-visible:ring-0 shadow-none resize-none' />
+                </div>
+            </div>
 
             {/* Suggestion List  */}
 
