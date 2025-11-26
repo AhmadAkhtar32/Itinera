017dacebe1508ecfe618ff2253bab7a2292c6eb7
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-11-26 18:59:04 +0500

    Table created and all setup done for User

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+"use cilent"
+import React, { useEffect } from 'react'
 import Header from './_components/Header';
+import { useMutation } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import { useUser } from '@clerk/nextjs';
 
 function Provider({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+
+    const CreateUser = useMutation(api.user.CreateNewUser)
+
+    const { user } = useUser();
+
+    useEffect(() => {
+        user && CreateNewUser();
+    }, [user])
+
+    const CreateNewUser = async () => {
+        if (user) {
+            // Save new user if not exist
+            const result = await CreateUser({
+                email: user?.primaryEmailAddress?.emailAddress ?? '',
+                imageUrl: user?.imageUrl,
+                name: user?.fullName ?? ''
+            })
+        }
+    }
+
     return (
         <div>
             <Header />
