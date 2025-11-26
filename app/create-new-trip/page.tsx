46a7714c36003c29f2130375e8de646e2e46a6f5
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-11-27 01:32:25 +0500

    Chatbox UI designed

--- app/create-new-trip/page.tsx
+++ app/create-new-trip/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import ChatBox from './_components/ChatBox'
 
 function CreateNewTrip() {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 gap-5 p-10'>
       <div>
-        chatBox
+        <ChatBox />
       </div>
       <div>
         map and trip plan to display
