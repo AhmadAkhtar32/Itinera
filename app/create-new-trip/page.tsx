b724e08e5a2690ceafbc467bc01976b18ec08cf9
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-11-27 00:55:10 +0500

    Inside new trip folder created folder for components

--- app/create-new-trip/page.tsx
+++ app/create-new-trip/page.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function CreateNewTrip() {
   return (
-    <div>Create New Trip</div>
+    <div className='grid grid-cols-1 md:grid-cols-2 gap-5 p-10'>
+      <div>
+        chatBox
+      </div>
+      <div>
+        map and trip plan to display
+      </div>
+    </div>
   )
 }
 
