c8180045cdc79f79c266ad0977716598cc551dee
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-11-29 01:37:44 +0500

    Final Ui Created

--- app/create-new-trip/_components/FinalUi.tsx
+++ app/create-new-trip/_components/FinalUi.tsx
@@ -1,8 +1,19 @@
+import { Button } from '@/components/ui/button'
+import { Globe2 } from 'lucide-react'
 import React from 'react'
 
-function FinalUi() {
+function FinalUi({ viewTrip }: any) {
     return (
-        <div>FinalUi</div>
+        <div>
+            <Globe2 className='text-primary text-4xl animate-bounce' />
+            <h2 className='mt-3 text-lg font-semibold text-primary'>
+                ✈️ Planning Your Dream trip ...
+            </h2>
+            <p className='text-gray-500 text-sm text-center mt-1'>
+                Gathering best destinations, activities and travel details for you.
+            </p>
+            <Button disabled onClick={viewTrip} className='mt-2 w-full'>View trip</Button>
+        </div>
     )
 }
 
