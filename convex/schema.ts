447e099f7ec6df8feb4771b9d322d7bfc0824fdf
AhmadAkhtar32 <ahmadrao3226@gmail.com>
2026-02-28 21:00:59 +0500

    Schema Updated for adding Favourite trips

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,7 +2,6 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-    // 1. Rename to UserTable (PascalCase is standard)
     Usertable: defineTable({
         name: v.string(),
         imageUrl: v.string(),
@@ -13,9 +12,7 @@ export default defineSchema({
     TripDetailTable: defineTable({
         tripId: v.string(),
         tripDetail: v.any(),
-
-        // 2. CRITICAL FIX: Change this to v.string()
-        // This allows you to store the Clerk User ID without errors.
         uid: v.string(),
+        isFavorite: v.optional(v.boolean()), // Added for favorites
     }),
 });
